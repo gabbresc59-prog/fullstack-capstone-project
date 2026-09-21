ef0332259a46f267e1380e41bd03832e0011b80a
gabbresc59-prog <gabbresc59@gmail.com>
2026-09-21 10:26:09 -0400

    Added search page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
         </Routes>
         </>
   );
